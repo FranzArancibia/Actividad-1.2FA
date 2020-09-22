7ea951f0b599f2cd095f9aa6d41c648ab7a742c6
FranzArancibia <f.arancibia13@alumnos.santotomas.cl>
2020-09-22 18:15:02 -0300

    Actividad Terminada con un TextView.

--- app/src/main/java/com/asignatura/actividad_12fa/Activity2.java
+++ app/src/main/java/com/asignatura/actividad_12fa/Activity2.java
@@ -1,14 +1,25 @@
 package com.asignatura.actividad_12fa;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class Activity2 extends AppCompatActivity {
+
+    Bundle rdatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        //Recibe los datos
+        rdatos = getIntent().getExtras();
+        // Convierte los datos en String
+        String datosOb = rdatos.getString("datos");
+        TextView mostrardatos = (TextView) findViewById((R.id.txtDestino));
+        //Muestra los datos
+        mostrardatos.setText(datosOb);
     }
 }
--- app/src/main/java/com/asignatura/actividad_12fa/MainActivity.java
+++ app/src/main/java/com/asignatura/actividad_12fa/MainActivity.java
@@ -4,18 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button Enviar;
+    EditText txtDat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtDat = (EditText) findViewById(R.id.txtDatos);
+        Enviar = (Button) findViewById(R.id.btnEnviar);
+        /// Implementacion de OnClickListener (su click rapido)///
+        Enviar.setOnClickListener(this);
     }
 
 
     public void enviarDatos(View v) {
 
+        //Convierte el EditText a String
+        String datos = txtDat.getText().toString();
+        // Crea Intent
+        Intent pasardatos = new Intent (MainActivity.this, Activity2.class);
+        //Envia Datos
+        pasardatos.putExtra("datos",datos);
+        //Inicia Activity2
+        startActivity(pasardatos);
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        enviarDatos(view);
     }
 }
